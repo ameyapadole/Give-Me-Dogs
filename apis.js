ff8dde751a9e17fb82a920b568bc51dca5596904
Ameya Padole <ameyapadole26@gmail.com>
2023-11-06 15:54:23 -0800

    First Commit

---
+++ apis.js
@@ -0,0 +1,35 @@
+const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+
+//promise is just an object. It represents a future value that we are trying to get back from the API. 
+//Think Sync. 
+
+const doggos = document.getElementById("dog-target");
+
+function addNewDoggos(){
+    const promise = fetch(DOG_URL);
+    promise.then(function(response) {
+
+        const processingPromise = response.text();
+        return processingPromise;
+
+        })
+
+        .then(function (processedPromise){
+
+        const dogObject = JSON.parse(processedPromise);
+        const img = document.createElement("img");
+        img.src = dogObject.message;
+        img.style.display = "block";
+        img.style.marginLeft ="auto";
+        img.style.marginRight = "auto";
+        img.style.width = "50%";
+        img.alt = "Cute Doggo";
+        doggos.appendChild(img);
+    }) 
+    .catch(function(error){
+        alert("API Error")
+    });
+}
+
+document.getElementById("dog-button").addEventListener("click", addNewDoggos);
+
